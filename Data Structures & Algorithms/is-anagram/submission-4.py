14b06cc14dc9cbfb60a938d693cfd67b6e8aee40
Volkov Maxim <44219431+volkov-maxim@users.noreply.github.com>
2026-08-27 17:31:28 +0300

    Delete Data Structures & Algorithms/is-anagram/submission-4.py

--- Data Structures & Algorithms/is-anagram/submission-4.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): 
-            return False
-        else:
-            return Counter(s) == Counter(t)
-            
