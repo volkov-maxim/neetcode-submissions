a11b2339ff07618977abd1e2c9538924d155b335
Volkov Maxim <44219431+volkov-maxim@users.noreply.github.com>
2026-08-30 11:00:47 +0300

    Delete Data Structures & Algorithms/is-palindrome/submission-2.py

--- Data Structures & Algorithms/is-palindrome/submission-2.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s_purified = "".join(char.lower() for char in s if char.isalnum())
-        return s_purified == s_purified[::-1]
