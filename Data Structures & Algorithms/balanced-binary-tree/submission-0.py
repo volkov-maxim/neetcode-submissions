4ca07ed556e5a20dad4a5f902032c128581e22b2
Volkov Maxim <44219431+volkov-maxim@users.noreply.github.com>
2026-09-12 12:10:02 +0300

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        res = True
+        def dfs(node) -> int:
+            """Calculates height/depth of tree."""
+            if not node: return 0
+            nonlocal res
+
+            left_height = dfs(node.left)
+            right_height = dfs(node.right)
+            if abs(left_height - right_height) > 1:
+                res = False
+
+            return 1 + max(left_height, right_height)
+        
+        dfs(root)
+        return res
